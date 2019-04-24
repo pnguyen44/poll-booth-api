9794a3c0699346ec14c7d75ecf56bfeaaa11d9bf
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 16:15:09 -0400

    Add test specs for Survey model

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Survey, type: :model do
+  # Association test
+  # ensure Survey model has a 1:m relationship with the Option model
+  it { should have have_many(:options).dependent(:destroy) }
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+
+  it { should have validate_presence_of(:title) }
+  it { should have validate_presence_of(:description) }
+  it { should have validate_presence_of(:question) }
+  it { should have validate_presence_of(:created_by) }
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
