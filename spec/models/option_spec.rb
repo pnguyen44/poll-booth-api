49962f75e21201d4b98f9c4f12a290bb4e08f2fa
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 16:22:32 -0400

    Add test suite for Option model

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+# Test suite for the Option model
 RSpec.describe Option, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  # Association test
+  # ensure an option record belongs to a single survey record
+  it { should belong_to(:survey) }
+
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:name) }
 end
