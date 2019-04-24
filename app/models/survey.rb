9052c41385b97f8cd4cd95fb468b3d4adf986e61
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 16:40:59 -0400

    Update Survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,8 @@
+# app/models/todo.rb
 class Survey < ApplicationRecord
+  # model association
+  has_many :options, dependent: :destroy
+
+  # validations
+  validate_presence_of(:title, :description, :question, :created_by)
 end
