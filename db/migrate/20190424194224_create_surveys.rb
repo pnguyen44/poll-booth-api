6bc18d330d93d364893df04822e379588fa9cc2d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 15:42:31 -0400

    Generate Survey model

---
+++ db/migrate/20190424194224_create_surveys.rb
@@ -0,0 +1,12 @@
+class CreateSurveys < ActiveRecord::Migration[5.1]
+  def change
+    create_table :surveys do |t|
+      t.string :title
+      t.string :description
+      t.string :question
+      t.string :created_by
+
+      t.timestamps
+    end
+  end
+end
