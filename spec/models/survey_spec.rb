ac81e78c1dab6a1064dd3569e4b78eadfdc51000
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 16:30:02 -0400

    Clean code

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Survey, type: :model do
   # ensure Survey model has a 1:m relationship with the Option model
   it { should have have_many(:options).dependent(:destroy) }
   # Validation tests
-  # ensure columns title, description, question, and created_by are present before saving
+  # ensure columns title, description, question, and created_by are present
+  # before saving
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
