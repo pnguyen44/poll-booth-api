855eff41d1b25df66a988a272ce9a191e53d575e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 19:28:53 -0400

    Update surveys controller   - add index method

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,10 @@
+# app/controllers/surveys_controller.rb
 class SurveysController < ApplicationController
+  before_action :set_survey, only: %i[show update destroy]
+
+  # GET /surveys
+  def index
+    @surveys = Survey.all
+    render json: @surveys
+  end
 end
