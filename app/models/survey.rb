83cb2fd8ff77fe333aea4b55e468237f5004e0b8
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-24 16:54:04 -0400

    Update survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,5 +4,5 @@ class Survey < ApplicationRecord
   has_many :options, dependent: :destroy
 
   # validations
-  validates_presence_of(:title, :description, :question, :created_by)
+  validates_presence_of :title, :description, :question, :created_by
 end
