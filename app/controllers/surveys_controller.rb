1b1140e1712b397928651d0a1d610d5e87a801c7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-04-25 17:04:32 -0400

    Update surveys controller   - update create function

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -7,4 +7,31 @@ class SurveysController < ApplicationController
     @surveys = Survey.all
     render json: @surveys
   end
+
+  # GET /surveys/1
+
+  def show
+    render json: @survey
+  end
+
+  # POST /surveys
+  def create
+    @survey = Survey.new(survey_params)
+
+    if @survey.save
+      render json: @survey, status: :created
+    else
+      render json: @survey.errors, status: :unprocessable_entity
+    end
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_survey
+    @survey = Survey.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def survey_params
+    params.require(:survey).permit(:title, :description, :question, :created_by)
+  end
 end
