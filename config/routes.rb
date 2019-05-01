3dfaf22be0e382b30b6d34b868b7a06d3f4bca1f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-01 18:09:18 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :surveys do
-    resources :options
-  end
+  # resources :surveys do
+  #   resources :options
+  # end
+  resources :options, except: %i[new edit]
+  resources :surveys, except: %i[new edit]
 end
