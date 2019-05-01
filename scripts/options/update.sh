9f01f6a30c392e74ad3b9f2225f7551836970854
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-01 19:45:05 -0400

    Update options update curl script

---
+++ scripts/options/update.sh
@@ -0,0 +1,19 @@
+ID=2
+NAME='!updateing....!!!!!!'
+VOTE_COUNT='11'
+
+API="http://localhost:3000"
+URL_PATH="/options/${ID}"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --data '{
+    "option": {
+      "name": "'"${NAME}"'",
+      "vote_count": "'"${VOTE_COUNT}"'"
+    }
+  }'
+
+echo
