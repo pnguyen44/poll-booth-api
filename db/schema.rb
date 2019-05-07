d4d2c5a80421f2ac1226c34427d8d8ea34925b66
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-07 16:56:03 -0400

    Ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507203526) do
+ActiveRecord::Schema.define(version: 20190507204836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20190507203526) do
 
   create_table "surveys", force: :cascade do |t|
     t.string "title"
-    t.string "description"
     t.string "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
