955536e28402783b24d693b8644255c9b91b7a1b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-08 19:16:46 -0400

    Update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,20 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+development_client_origin = 'http://localhost:3000' if Rails.env.development
+
+development_client_origin ||= ENV['CLIENT_ORIGIN']
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins do |origin, _env|
+      ENV['CLIENT_ORIGIN'] == '*' ||
+        origin == ENV['CLIENT_ORIGIN'] ||
+        origin == development_client_origin
+    end
+    resource '*',
+             headers: :any,
+             methods: %i[options head get delete patch put post]
+  end
+end
